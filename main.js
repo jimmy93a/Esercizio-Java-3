acdb457dceece28675109d3db99eda7c641b36fd
Jimmy Albano <jimmy93a@gmail.com>
2025-12-10 17:57:33 +0100

    Mi ero dimenticato la potenza

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ let somma = num1 + num2;
 let sottrazzione = num1 - num2;
 let moltiplicazione = num1 * num2;
 let divisione = num1 / num2;
+let potenza = num1 ** num2;
 
-console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma}, sottrazione ${sottrazzione}, moltiplicazione ${moltiplicazione}, divisione ${divisione}`);
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma}, sottrazione ${sottrazzione}, moltiplicazione ${moltiplicazione}, divisione ${divisione}, potenza ${potenza}`);
