cb6e3a4e6dfdd7a926ac779d6fafeb77b8aaed30
Jimmy Albano <jimmy93a@gmail.com>
2025-12-10 17:32:10 +0100

    Inizio esercizio 3 javascript

---
+++ main.js
@@ -0,0 +1,9 @@
+let num1 = parseFloat(prompt("Inserisci il primo numero:"));
+let num2 = parseFloat(prompt("Inserisci il secondo numero:"));
+
+let somma = num1 + num2;
+let sottrazzione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma ${somma}, sottrazione ${sottrazzione}, moltiplicazione ${moltiplicazione}, divisione ${divisione}`);
